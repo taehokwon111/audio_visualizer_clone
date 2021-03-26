722f9819e336dc96c4f0e7fe63049f29e5ac428b
Cancermaker <52158699+Cancermaker@users.noreply.github.com>
2021-03-26 19:54:26 +0900

    add

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function handleFiles(event) {
 }
     
 function test() {
-    audio.pause();
+    audio.load();
 }
     
 
---
+++ js/move.js
@@ -0,0 +1,31 @@
+// let x = 0;
+// let y = 0;
+// let mx = 0;
+// let my = 0;
+// let speed = 0.009;
+// let model;
+// window.onload = function () {
+//     model = document.getElementsByClassName("test")[0];
+  
+//     window.addEventListener("mousemove", mouseFunc, false);
+//     alert("dsd")
+//     function mouseFunc(e) { 
+//         x = (e.clientX - window.innerWidth / 2);
+//         y = (e.clientY - window.innerHeight / 2);
+//     }
+
+//     loop();
+// }
+
+// function loop() {
+//     mx += (x - mx) * speed;
+//     my += (y - my) * speed;
+
+//     h1.innerHTML = "x: " + x + " mx: " + mx;
+
+//     model.style.transform = "translate("+ (mx/6)+"px," + (my/6)+"px)";
+
+
+// window.requestAnimationFrame(loop); 
+
+// }
